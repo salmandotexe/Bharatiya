8249f6f83090dcc7bdbd5329bd0b5b66b229ba5a
Salman Sahel <salman.exe@gmail.com>
2020-12-24 19:09:53 +0600

    Test

--- app/src/main/java/com/kms/bharatiya/MainActivity.java
+++ app/src/main/java/com/kms/bharatiya/MainActivity.java
@@ -68,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
 
+
+                
                 /*
                     Add markers using this:
                     List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
