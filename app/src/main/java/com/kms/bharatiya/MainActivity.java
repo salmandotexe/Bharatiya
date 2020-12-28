87278eff8f0ea8b196dd83cc0d19f102a73f9484
Salman Sahel <salman.exe@gmail.com>
2020-12-28 20:36:07 +0600

    Implemented House Markers Successfully
    Used StyleLayer to add Markers based on GeoJson + Lat/Long coordinates.

--- app/src/main/java/com/kms/bharatiya/MainActivity.java
+++ app/src/main/java/com/kms/bharatiya/MainActivity.java
@@ -2,15 +2,40 @@ package com.kms.bharatiya;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.*;
 
+//Additional imports for Markers:
+
+import android.graphics.BitmapFactory;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
+//import com.mapbox.mapboxandroiddemo.R; not found
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+
+
+
+//Java imports:
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private String curstl= "mapbox://styles/thesalmansahel/ckh50xh0w00se19pfe4krhrxa";
     private MapView mapView;
+
+    //For icons of map markers
+    private static final String SOURCE_ID = "SOURCE_ID";
+    private static final String ICON_ID = "ICON_ID";
+    private static final String LAYER_ID = "LAYER_ID";
+    private List<Feature> HouseList = new ArrayList<>();    //Contains list of houses where markers need to be placed.
     @Override
     protected void onStart() {
         super.onStart();
@@ -67,27 +92,42 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                HouseList.add(Feature.fromGeometry(
+                    Point.fromLngLat(90.414208, 23.779395)));    //Three points in Gulshan-1
+                HouseList.add(Feature.fromGeometry(
+                    Point.fromLngLat(90.416990, 23.778934)));
+                HouseList.add(Feature.fromGeometry(
+                    Point.fromLngLat(90.417402,23.781153)));
 
+                //TODO: Use a getCoordinates() function here that loads the Lat/Long from the Database and adds to HouseList.
 
-                
-                /*
-                    Add markers using this:
-                    List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
-                    symbolLayerIconFeatureList.add(Feature.fromGeometry(
-                    Point.fromLngLat(-57.225365, -33.213144)));
-                    symbolLayerIconFeatureList.add(Feature.fromGeometry(
-                    Point.fromLngLat(-54.14164, -33.981818)));
-                    symbolLayerIconFeatureList.add(Feature.fromGeometry(
-                    Point.fromLngLat(-56.990533, -30.583266)));
+                mapboxMap.setStyle(new Style.Builder().fromUri(curstl)
+                //Note: Icons are added on a separate layer, as an overlay above the map.
 
-                 */
+                //Add icon image for marker. DO NOT CHANGE ICON_ID at the moment.
+                .withImage(ICON_ID, BitmapFactory.decodeResource( MainActivity.this.getResources(), R.drawable.mapbox_marker_icon_default))
 
-                mapboxMap.setStyle(new Style.Builder().fromUri(curstl), new Style.OnStyleLoaded() {
+                //Place the marker icons above the houses for rent, GeoJson translates lat long into map coordinates.
+                .withSource(new GeoJsonSource(SOURCE_ID, FeatureCollection.fromFeatures(HouseList)))
+
+                // Creating the actual Layer as overlay, and adding an offset to the icon so the marker arrow points at the house.
+
+                .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
+                    .withProperties(
+                        iconImage(ICON_ID),
+                        iconAllowOverlap(true),
+                        iconIgnorePlacement(true)
+                    )
+                ),
+
+                new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
 
                     }
                 });
+
+
 
             }
         });
